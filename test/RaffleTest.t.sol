ad763466d0e12053dbb5c85ff57bdbb5a5d3580b
Aditya <rajaditya3759@gmail.com>
2023-08-09 14:43:58 +0530

    Test: Entre raffle is tested

--- test/RaffleTest.t.sol
+++ test/RaffleTest.t.sol
@@ -10,7 +10,9 @@ import {HelperConfig} from "../script/HelperConfig.s.sol";
 contract RaffleTest is Test {
     Raffle raffle;
     HelperConfig helperConfig;
-    address PLAYER = makeAddr("player");
+    address PLAYER1 = makeAddr("player1");
+    address PLAYER2 = makeAddr("player2");
+    address PLAYER3 = makeAddr("player3");
 
     uint256 entranceFee;
     uint256 interval;
@@ -32,8 +34,11 @@ contract RaffleTest is Test {
         //     subscriptionId,
         //     callbackGasLimit
         // ) = helperConfig.activeNetwork();
-        // vm.deal(PLAYER, 10e18);
-        raffle = Raffle(0xa9CCc63fC43f6Fce418D09f23A4Dd7B2f726637c);
+        raffle = Raffle(0x99fd723c10c490fb1Ba8b66e1dEFbb4DD0c7DbDb);
+        vm.deal(PLAYER1, 10e18);
+        vm.deal(PLAYER2, 10e18);
+        vm.deal(PLAYER3, 10e18);
+        console.log(address(raffle));
     }
 
     function testRaffleInitialStateIsOpen() public view {
@@ -41,85 +46,53 @@ contract RaffleTest is Test {
     }
 
     function testEnterRaffleFailIfNotEnoughEth() public {
-        vm.prank(PLAYER);
+        vm.prank(PLAYER1);
         vm.expectRevert();
         raffle.enterRaffle();
     }
 
     function testRaffleStorePlayersWhenEnter() public {
-        vm.prank(PLAYER);
+        vm.prank(PLAYER1);
         raffle.enterRaffle{value: 0.01 ether}();
 
         address player = raffle.s_player(0);
-        assert(player == PLAYER);
+        assert(player == PLAYER1);
     }
 
     function testRaffleEmitEventWhenPlayerEntered() public {
-        vm.prank(PLAYER);
+        vm.prank(PLAYER1);
         vm.expectEmit(address(raffle));
-        emit RaffleEntered(PLAYER);
+        emit RaffleEntered(PLAYER1);
         raffle.enterRaffle{value: 0.01 ether}();
     }
 
-    // function testCantEnterRaffleWhenItIsCalculating() public {
-    //    vm.prank(PLAYER);
-    //    raffle.enterRaffle{value: 0.01 ether}();
+    function testCantEnterRaffleWhenItIsCalculating() public {
+        vm.prank(PLAYER1);
+        raffle.enterRaffle{value: 0.01 ether}();
+        vm.prank(PLAYER2);
+        raffle.enterRaffle{value: 0.01 ether}();
+        vm.prank(PLAYER3);
+        raffle.enterRaffle{value: 0.01 ether}();
 
-    //    vm.warp(block.timestamp + interval + 1);
-    //    vm.roll(block.number + 1);
+        vm.warp(block.timestamp + interval + 1);
+        vm.roll(block.number + 5);
 
-    //    raffle.
+        raffle.pickWinner();
+
+        vm.prank(PLAYER1);
+        vm.expectRevert();
+        raffle.enterRaffle{value: 0.01 ether}();
+    }
+
+    // function testPickWinnerFailsIfTimeNotPassed() public {
+    //     for (uint160 i = 1; i < 5; i++) {
+    //         hoax(address(i), 5e18);
+    //         raffle.enterRaffle{value: 0.01 ether}();
+    //     }
+    //     console.log(block.timestamp);
+    //     console.log(raffle.lastTimeStamp());
+    //     vm.prank(PLAYER1);
+    //     // vm.expectRevert();
+    //     raffle.pickWinner();
     // }
-
-    function testCheckUpKeepReturnsFalseIfTimeHasNotPassed() public {
-        for (uint160 i = 1; i < 5; i++) {
-            hoax(address(i), 5e18);
-            raffle.enterRaffle{value: 0.01 ether}();
-        }
-
-        (bool upKeepNeeded, ) = raffle.checkUpkeep("");
-        assert(upKeepNeeded == false);
-    }
-
-    function testCheckUpKeepReturnsFalseIfPlayerAndBalanceIsNotGreaterThanThreeAndZero()
-        public
-    {
-        vm.prank(PLAYER);
-        raffle.enterRaffle{value: 0.01 ether}();
-
-        vm.warp(block.timestamp + interval + 1);
-        vm.roll(block.number + 1);
-
-        (bool upKeepNeeded, ) = raffle.checkUpkeep("");
-        assert(upKeepNeeded == false);
-    }
-
-    function testCheckUpKeepReturnsTrueWhenAllAreTrue() public {
-        for (uint160 i = 1; i < 5; i++) {
-            hoax(address(i), 5e18);
-            raffle.enterRaffle{value: 0.01 ether}();
-        }
-
-        vm.warp(block.timestamp + interval + 1);
-        vm.roll(block.number + 1);
-
-        (bool upKeepNeeded, ) = raffle.checkUpkeep("");
-        assert(upKeepNeeded == true);
-    }
-
-    function testPlayersEnterAndWinnerGotSelected() public {
-        for (uint160 i = 1; i < 5; i++) {
-            hoax(address(i), 5e18);
-            raffle.enterRaffle{value: 0.01 ether}();
-        }
-
-        vm.warp(block.timestamp + interval + 1);
-        vm.roll(block.number + 1);
-        uint256 initialRaffleBalance = address(raffle).balance;
-        console.log(initialRaffleBalance);
-
-        (bool upKeepNeeded, ) = raffle.checkUpkeep("");
-        uint256 finalRaffleBalance = address(raffle).balance;
-        console.log(finalRaffleBalance);
-    }
 }
